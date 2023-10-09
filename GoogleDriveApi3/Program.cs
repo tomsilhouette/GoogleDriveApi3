d6de7c7eed102dce25f3bba92fd4b032242e4921
tomsilhouette <tbrockington@silhouettesoftware.com>
2023-10-09 11:29:39 +0100

    working download

--- GoogleDriveApi3/Program.cs
+++ GoogleDriveApi3/Program.cs
@@ -9,6 +9,7 @@ using Newtonsoft.Json;
 using System.Diagnostics;
 using System.Net.Http.Headers;
 using System.IO;
+using System.Net;
 
 namespace GoogleDriveApi3
 {
@@ -24,7 +25,6 @@ namespace GoogleDriveApi3
         public static async void GetArtifact()
         {
             string apiUrl = "https://dev.azure.com/SilhouetteResearch/Silhouette%20Suite/_apis/build/builds/1221/artifacts?artifactName=Silhouette%20Go%20Android&api-version=4.1";
-            string apiUrlGet = "https://quiz-5cpoinrp2-webdesignbytom.vercel.app/users/all-users";
 
             var httpClient = new HttpClient();
 
@@ -66,10 +66,13 @@ namespace GoogleDriveApi3
             httpClient.DefaultRequestHeaders.Authorization =
                 new AuthenticationHeaderValue("Basic", "Onp5ZGo2ZHU3bGh3aHpnbm8yd3ZmZGxsZWV6dm1pbzNiZndwZ2xpczN1bzduNDRmZTR2aWE=");
 
+
             try
             {
                 HttpResponseMessage response = await httpClient.GetAsync(downloadUrl);
                 response.EnsureSuccessStatusCode(); // Ensure the request was successful
+
+                Console.WriteLine($"333333333 {response}");
 
                 // Get the response content as a stream
                 using (var stream = await response.Content.ReadAsStreamAsync())
@@ -83,9 +86,7 @@ namespace GoogleDriveApi3
 
                 Console.WriteLine("Download completed.");
 
-                string downloadUrl2 = "https://artproduks1.artifacts.visualstudio.com/Ad6968979-1781-402c-937c-af71d258be14/d8c71310-2a98-4ad2-9121-caafe8bf03df/_apis/artifact/cGlwZWxpbmVhcnRpZmFjdDovL1NpbGhvdWV0dGVSZXNlYXJjaC9wcm9qZWN0SWQvZDhjNzE3MTAtMmE5OC00YWQyLTkxMjEtY2FhZmU4YmYwM2RmL2J1aWxkSWQvMTIyMS9hcnRpZmFjdE5hbWUvU2lsaG91ZXR0ZStHbytBbmRyb2lk0/content?format=zip";
-
-                OpenUrlInBrowser(downloadUrl2);
+                OpenUrlInBrowser(downloadUrl);
             }
             catch (Exception ex)
             {
@@ -96,6 +97,7 @@ namespace GoogleDriveApi3
         public static void OpenUrlInBrowser(string url)
         {
             Console.WriteLine("CCCCCCCCCCCCCCCCCCCCCCCCCCCCCCCCCC");
+
             try
             {
                 // Use Process.Start to open the URL in the default web browser
@@ -103,10 +105,8 @@ namespace GoogleDriveApi3
             }
             catch (Exception ex)
             {
-                Console.WriteLine($"Error: {ex.Message}");
+                Console.WriteLine($"XXXX Error: {ex.Message}");
             }
         }
-
-
     }
 }
